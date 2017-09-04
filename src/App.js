ddbf11dd6db96ab114bb4bc00cfb336d44c59258
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2017-09-04 11:57:33 +0545

    Remove multiple css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
 
@@ -13,7 +12,7 @@ class App extends Component {
       </div>
     );
   }
-  
+
 }
 
 export default App;
